221b3fa76c0c6aff6ab1e95b7cf460d9dcb64314
ari123aji <65709320+ari123aji@users.noreply.github.com>
2021-12-30 16:09:44 +0700

    Update save.php

--- save.php
+++ save.php
@@ -1,24 +1,34 @@
 <?php
-
-require'connection.php';
+require 'connection.php';
 if(isset($_POST["save"])){
-    $sku = htmlspecialchars($_POST["sku"]);
-    $name = htmlspecialchars($_POST["name"]);
-    $price = htmlspecialchars($_POST["price"]);
-    $productType = htmlspecialchars($_POST["productType"]);
-    $size = htmlspecialchars($_POST["size"]);
-    $weight = htmlspecialchars($_POST["weight"]);
-    $height = htmlspecialchars($_POST["height"]);
-    $width = htmlspecialchars($_POST["width"]);
-    $length = htmlspecialchars($_POST["length"]);
+    $sku = $_POST["sku"];
+    $name = $_POST["name"];
+    $price = $_POST["price"];
+    $productType = $_POST["productType"];
+    $size = $_POST["size"];
+    $weight = $_POST["weight"];
+    $height = $_POST["height"];
+    $width = $_POST["width"];
+    $length = $_POST["length"];
 
-    $query = "INSERT INTO product
-        Values
-    ('', '$sku', '$name', '$price', '$productType', '$size', '$weight', '$height', '$width', '$length')";
-    mysqli_query($conn, $query);
+    $query = "INSERT INTO product(sku, name, price, productType, size, weight, height, width, length)
+        VALUES (
+        '$sku',
+        '$name',
+        '$price',
+        '$productType',
+        '$size',
+        '$weight',
+        '$height',
+        '$width',
+        '$length')";
+    $result = mysqli_query($conn,$query);
+    if ($result) {
+        header("Location:index.php");
+        return mysqli_affected_rows($conn);        
+    } else {
+        echo "error";
+    }
 
-    header("Location:index.php");
-    return mysqli_affected_rows($conn);
 }
-
 ?>
